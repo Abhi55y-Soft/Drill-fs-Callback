923b10a3c6b3308ee7964d760842f6560e61deba
Abhishek Yadav <abhishek.yadav.17@mountblue.tech>
2021-09-16 10:59:05 +0530

    modified with correction

--- problem2.js
+++ problem2.js
@@ -2,34 +2,73 @@ const fs = require('fs');
 
 const problem2 = () => {
     let path = 'lipsum.txt';
-    fs.readFile(path, 'utf8', function (err, data) {
-        let upperCaseData = data.toUpperCase();
-        fs.writeFile('upperCase.txt', upperCaseData, function(err, result) {
-            if(err) console.log('error', err);
-            const lowerCaseData = upperCaseData.toLowerCase();
-            let sentences = lowerCaseData.split('.');
-            sentences.forEach(element => {
-                fs.appendFile('lowerCase.txt', element+"\n", function(err, result) {
-                    if(err) console.log('error', err);
+    fs.readFile(path, 'utf8', (err, data)=> {
+        if(err){
+            console.log('error', err);
+        }else{
+            let upperCaseData = data.toUpperCase();
+            fs.writeFile('upperCase.txt', upperCaseData, (err, data)=> {
+                fs.writeFile('filenames.txt', 'upperCase.txt', (err, data)=> {
+                    if (err){
+                        console.log('error', err);
+                    }
                 });
-            });
-            fs.readFile('lowerCase.txt', 'utf8', function (err, data){
-                sentences.sort();
-                sentences.forEach(element => {
-                    fs.appendFile('sorted.txt', element+"\n", function(err, result) {
-                        if(err) console.log('error', err);
+                if (err){
+                    console.log('error', err);
+                }else{
+                    fs.readFile('upperCase.txt', 'utf8', (err, data)=> {
+                        if (err){
+                            console.log('error', err);
+                        }else{
+                            let lowerCaseData = data.toLowerCase().replace(/([.?!])\s*(?=[A-Z])/g, "$1|").split("|").join("\n");
+                            fs.writeFile('lowerCase.txt', lowerCaseData, (err, data)=> {
+                                fs.appendFile('filenames.txt', ' lowerCase.txt', (err, data)=> {
+                                    if (err){
+                                        console.log('error', err);
+                                    }
+                                })
+                                if (err){
+                                    console.log('error', err);
+                                }else{
+                                    fs.readFile('lowerCase.txt', 'utf8', (err, data)=> {
+                                        if (err){
+                                            console.log('error', err);
+                                        }else{
+                                            let sortedData = data.split("\n").sort().join("\n");
+                                            fs.writeFile('sorted.txt', sortedData, (err, data)=> {
+                                                fs.appendFile('filenames.txt', ' sorted.txt', (err, data)=> {
+                                                    if (err){
+                                                        console.log('error', err);
+                                                    }
+                                                });
+                                                if (err){
+                                                    console.log('error', err);
+                                                }else{
+                                                    fs.readFile('filenames.txt', 'utf8', (err, data)=> {
+                                                        if (err){
+                                                            console.log('error', err);
+                                                        }else{
+                                                            let listOfNames = data.split(' ');
+                                                            listOfNames.forEach((element)=> {
+                                                                fs.unlink(element, (err)=> {
+                                                                    if (err){
+                                                                        console.log('error', err);
+                                                                    }
+                                                                })
+                                                            });
+                                                        }
+                                                    });
+                                                }
+                                            });
+                                        }
+                                    });
+                                }
+                            });
+                        }
                     });
-                });
+                }
             });
-            fs.readFile('sorted.txt', 'utf8', function (err, data){
-                fs.unlink('upperCase.txt', (err) => {
-                    if (err) throw err;
-                    fs.unlink('lowerCase.txt', (err) => {
-                        if (err) throw err;
-                    });
-                });
-            });
-        });
+        }
     });
 }
 
